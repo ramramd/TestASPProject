994deb4cd5ee16a01876ed48a2e4d847cec725db
Naveen Ramachandrappa <naveen.ramachandrappa@limeade.com>
2020-01-30 11:36:36 -0800

    Initial check-in of module dontetCoreProject

---
+++ dontetCoreProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dontetCoreProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
